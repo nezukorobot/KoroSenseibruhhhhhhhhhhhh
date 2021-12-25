92af80a0eadbd676a87f34f68c363dc019938438
nezukorobot <93088904+nezukorobot@users.noreply.github.com>
2021-12-25 08:41:06 +0530

    Update country.py

--- EmikoRobot/modules/country.py
+++ EmikoRobot/modules/country.py
@@ -101,7 +101,7 @@ Time Zones:- {tom}
 Top Level Domain:- {lanester}
 wikipedia:- {wiki}</b>
 
-Gathered By Emiko Robot ✨.</b>
+Gathered By Lelouch Robot ❤️.</b>
 """
 
     await borg.send_message(
