096f212582572723a3236d92a46324ac58f5cdd6
DivinlyDiable12 <100528615+DivinlyDiable12@users.noreply.github.com>
2022-03-21 15:26:53 +0530

    afk.py

--- EmikoRobot/modules/afk.py
+++ EmikoRobot/modules/afk.py
@@ -46,7 +46,7 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now away!{}".format(fname, notice))
+        update.effective_message.reply_text("{} hn hn ja ja kisi ko teri yaad nahi ayegi{}".format(fname, notice))
     except BadRequest:
         pass
 
