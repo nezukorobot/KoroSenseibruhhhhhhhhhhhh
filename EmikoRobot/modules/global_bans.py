4b7cb8db46f29b6e3c2504db0b49ae0b3840516f
nezukorobot <93088904+nezukorobot@users.noreply.github.com>
2022-07-02 12:38:29 +0530

    Update global_bans.py

--- EmikoRobot/modules/global_bans.py
+++ EmikoRobot/modules/global_bans.py
@@ -97,22 +97,22 @@ def gban(update: Update, context: CallbackContext):
 
     if int(user_id) in DRAGONS:
         message.reply_text(
-            "This person has Lelouch supremacy..",
+            "This person has Koro Sensei supremacy..",
         )
         return
 
     if int(user_id) in DEMONS:
         message.reply_text(
-            "This person has Lelouch supremacy.. *grabs popcorn*",
+            "This person has Koro Sensei supremacy.. *grabs popcorn*",
         )
         return
 
     if int(user_id) in TIGERS:
-        message.reply_text("This person has Lelouch supremacy..!! They cannot be banned!")
+        message.reply_text("This person has Koro Sensei supremacy..!! They cannot be banned!")
         return
 
     if int(user_id) in WOLVES:
-        message.reply_text("This person has Lelouch supremacy..")
+        message.reply_text("This person has Koro Sensei supremacy..")
         return
 
     if user_id == bot.id:
