0116e81ad8aa9e1a419c17713942960d42052cdb
nezukorobot <93088904+nezukorobot@users.noreply.github.com>
2022-07-02 12:46:49 +0530

    Update logomaker.py

--- EmikoRobot/modules/logomaker.py
+++ EmikoRobot/modules/logomaker.py
@@ -252,7 +252,7 @@ LOGO_LINKS            = ["https://telegra.ph/file/d1838efdafce9fe611d0c.jpg",
 async def lego(event):
  quew = event.pattern_match.group(1)
  if event.sender_id != OWNER_ID and not quew:
-  await event.reply('`Please give me the text for the logo!`\n`Example /logo <pantek>`')
+  await event.reply('`Please give me the text for the logo!`\n`Example /logo <Koro Sensei>`')
   return
  pesan = await event.reply('`Creating your logo...`')
  try:
@@ -264,7 +264,7 @@ async def lego(event):
     pointsize = 500
     fillcolor = "black"
     shadowcolor = "blue"
-    fnt = glob.glob("./Tianabot/resources/font/*")
+    fnt = glob.glob("./EmikoRobot/resources/font/*")
     randf = random.choice(fnt)
     font = ImageFont.truetype(randf, 120)
     w, h = draw.textsize(text, font=font)
@@ -276,7 +276,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=1, stroke_fill="black")
     fname = "Tiana.png"
     img.save(fname, "png")
-    await telethn.send_file(event.chat_id, file=fname, caption = f"Made by [Lelouch](@LelouchXRobot)")         
+    await telethn.send_file(event.chat_id, file=fname, caption = f"Made by [KoroSensei](@KoroXSensei_bot)")         
     await pesan.delete()
     if os.path.exists(fname):
             os.remove(fname)
